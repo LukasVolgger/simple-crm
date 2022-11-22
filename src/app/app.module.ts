3eaf7505efd02b73e261e478b76db0cacd24aa62
Lukas Volgger <lukas.volgger@live.at>
2022-11-22 13:06:49 +0100

    add sidenav to toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,9 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     BrowserAnimationsModule,
     // Angular Material - BEGIN
     MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule
     // Angular Material - END
   ],
   providers: [],
