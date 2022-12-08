d66a291e779d3f3898ee8bc7e63be2b9f023d53d
Lukas Volgger <lukas.volgger@live.at>
2022-12-08 11:20:17 +0100

    validate login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from 'src/app/services/auth.service';
 import { DialogGuestUserComponent } from '../dialog-guest-user/dialog-guest-user.component';
@@ -11,12 +12,27 @@ import { DialogGuestUserComponent } from '../dialog-guest-user/dialog-guest-user
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
   constructor(
     public authService: AuthService,
-    public dialog: MatDialog) { }
+    public dialog: MatDialog,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  /**
+   * Submits a form
+   */
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.signIn(this.loginForm.value.email, this.loginForm.value.password);
+    }
   }
 
   /**
