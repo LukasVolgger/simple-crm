6bef9e09e5ae92a073c9b3ddc80eb8aca7e3e7ff
Lukas Volgger <lukas.volgger@live.at>
2022-12-09 10:39:42 +0100

    validate edit user form

--- src/app/components/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/components/dialog-edit-user/dialog-edit-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { AuthService } from 'src/app/services/auth.service';
 import { FirestorageService } from 'src/app/services/firestorage.service';
@@ -10,15 +11,30 @@ import { DialogDeleteUserComponent } from '../dialog-delete-user/dialog-delete-u
   styleUrls: ['./dialog-edit-user.component.scss']
 })
 export class DialogEditUserComponent implements OnInit {
+  editUserForm!: FormGroup;
 
   constructor(
     public authService: AuthService,
     public firestorageService: FirestorageService,
     public dialog: MatDialog,
     public dialogRef: MatDialogRef<DialogEditUserComponent>,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.editUserForm = this.fb.group({
+      newDisplayName: ['', Validators.minLength]
+    });
+  }
+
+  /**
+   * Submits a form
+   */
+  onSubmit() {
+    if (this.editUserForm.valid) {
+      this.authService.changeDisplayName(this.authService.newDisplayName);
+      this.dialogRef.close();
+    }
   }
 
   /**
