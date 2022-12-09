8652b83f3efbc6efa06ca35fb5cf328545d3a205
Lukas Volgger <lukas.volgger@live.at>
2022-12-09 11:44:30 +0100

    forgot-password form validation and minor improvement to verify email

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 
 
@@ -9,10 +10,26 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  forgotPasswordForm!: FormGroup;
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', Validators.required],
+    });
+  }
+
+  /**
+   * Submits a form
+   */
+  onSubmit() {
+    if (this.forgotPasswordForm.valid) {
+      this.authService.forgotPassword(this.forgotPasswordForm.value.email);
+    }
   }
 
 }
