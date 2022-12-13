72df73ef31aae3afdd34c28727a6f4678aab25d8
Lukas Volgger <lukas.volgger@live.at>
2022-12-13 15:45:51 +0100

    make main route unreachable and more style improvements

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if (this.router.url == '/main') {
+      this.router.navigateByUrl('/main/dashboard');
+    }
   }
 
 }
