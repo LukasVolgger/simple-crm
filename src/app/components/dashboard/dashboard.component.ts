23ac0b33b820f0b07e89ee5485f25dbf851b922a
Lukas Volgger <lukas.volgger@live.at>
2022-12-25 11:34:46 +0100

    add customers card to dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +8,29 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild('customersAmount') customersAmount!: ElementRef;
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    public firestoreService: FirestoreService,
+  ) { }
 
   ngOnInit(): void {
+    this.updateCustomers();
+  }
+
+  /**
+   * Calls the getAllCustomers() function and updates the template
+   */
+  updateCustomers() {
+    this.firestoreService.getAllCustomers();
+
+    let interval = setInterval(() => {
+      if (this.firestoreService.customers != undefined) {
+        clearInterval(interval);
+        this.customersAmount.nativeElement.innerHTML = this.firestoreService.customers.length;
+      }
+    }, 1000 / 60);
   }
 
 }
