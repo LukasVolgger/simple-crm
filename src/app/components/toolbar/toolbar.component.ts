70c4b1135615712c66bc0e1dffe813eed3543e71
Lukas Volgger <lukas.volgger@live.at>
2022-12-26 10:30:47 +0100

    add responsive design methods for toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatDrawer } from '@angular/material/sidenav';
 import { AuthService } from 'src/app/services/auth.service';
 import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
 import { DialogUserDetailsComponent } from '../dialog-user-details/dialog-user-details.component';
@@ -11,12 +12,50 @@ import { DialogUserDetailsComponent } from '../dialog-user-details/dialog-user-d
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, AfterViewInit {
+  @ViewChild('drawer') drawer!: MatDrawer;
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.applyResponsiveNav();
+  };
 
-  constructor(public authService: AuthService,
-    private dialog: MatDialog) { }
+  mobileNav: boolean = false;
+
+  constructor(
+    public authService: AuthService,
+    private dialog: MatDialog
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.applyResponsiveNav();
+  }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Toggle the MatDrawer in Mobile View
+   */
+  toggleNav() {
+    if (this.mobileNav) {
+      this.drawer.toggle();
+      this.drawer.mode = 'over';
+    }
+  }
+
+  /**
+   * Checks the window.innerheight and enables/disables the responsive view accordingly 
+   */
+  applyResponsiveNav() {
+    if (window.innerWidth < 768) {
+      this.drawer.close();
+      this.mobileNav = true;
+      this.drawer.mode = 'over';
+    } else {
+      this.drawer.open();
+      this.mobileNav = false;
+      this.drawer.mode = 'side';
+    }
   }
 
   /**
